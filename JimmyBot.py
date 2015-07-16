f976bd6125dc068b2dd4371f1ed7b014ba162219
Arian Jafari <arian91@hotmail.com>
2015-07-16 20:56:08 +0200

    Added better error handling
    Jimmy now responds with nicer error messages when he  hits a
    disambiguation page, and when he can't find a page at all.

--- JimmyBot.py
+++ JimmyBot.py
@@ -3,9 +3,12 @@ import json
 import wikipedia
 import pickle
 from time import sleep
+from wikipedia import DisambiguationError
+from wikipedia import PageError
 
 class JimmyBot:
 	def __init__(self, token):
+		print("Starting...")
 		self.token = token
 		self.db_path = open('db.pkl', 'rb')
 		self.db = pickle.load(self.db_path)
@@ -23,10 +26,24 @@ class JimmyBot:
 		
 	def handle_message(self, id, message):
 		print("Handle message: " + message)
-		query = message[5:]
-		wiki_response = wikipedia.summary(query, sentences = 3)
+		query = message.partition(' ')[2] 	# Get the stuff after the space
+		
+		try:
+			wiki_response = wikipedia.summary(query, sentences = 3)
+		except PageError:
+			wiki_response = "Sorry! Couldn't find any page matching \"" + query + "\" - please try another search term."
+		except DisambiguationError as e:
+			wiki_response = self.handle_disambiguation(e)
 		
 		self.send_message(id, wiki_response, self.url)
+		
+	def handle_disambiguation(self, e):
+		suggestions = e.options[0:3]
+		response = "Sorry! This search term leads me to a disambiguation page - please try a more precise search term.\nExamples:\n"
+		for suggestion in suggestions:
+			response += '- ' + suggestion + '\n'
+		
+		return response
 	
 	def update_last_update(self, last_update):
 		self.db = {"last_update" : last_update}
@@ -43,7 +60,7 @@ class JimmyBot:
 		self.db_path.close()
 		
 	def run(self):
-		print("Starting...")
+		print("Started.")
 		while True:
 			response = requests.get(self.url + 'getUpdates')
 			result = json.loads(response.text)
